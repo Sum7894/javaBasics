8f5513afd22ad8d183856f34208e8db6da5f8b7a
Sum7894 <159606495+Sum7894@users.noreply.github.com>
2024-02-17 17:17:09 +0530

    Program for counting occurrences of words in a String

--- BasicJavaPrep/src/main/java/org/example/Main.java
+++ BasicJavaPrep/src/main/java/org/example/Main.java
@@ -5,22 +5,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
-/** Count occurrences of charcters in a string
- * for eg : ababeu -  a = 2 , b = 2, e = 1, u = 1
- *
- * **/
-
-
-
 public class Main {
     public static void main(String[] args) {
         Scanner sin = new Scanner(System.in);
         String inputSt = sin.nextLine();
         Main m = new Main();
-        m.countOcc(inputSt.trim());
+        m.countOcc(inputSt);
+        m.countWords(inputSt);
 
     }
 
+    /** Count occurrences of charcters in a string
+     * for eg : ababeu -  a = 2 , b = 2, e = 1, u = 1
+     *
+     * **/
     public void countOcc(String str){
         Map<Character, Integer> counterMap = new HashMap<>();
         char[] chArr = str.toCharArray();
@@ -35,5 +33,25 @@ public class Main {
         }
          counterMap.remove(' ');
          System.out.println("MAP  : "+counterMap);
+    }
+
+    /** Count occurrences of words in a string using HashMap
+     * for eg :   Input: ab cd ab
+     *              O/P: - Occurrences of Words: ab = 2, cd = 1
+     *
+     * **/
+    public void countWords(String str){
+        String[] wordsArr = str.split(" ");
+        Map<String, Integer> counterMap = new HashMap<>();
+        for(String st : wordsArr ){
+            if(counterMap.containsKey(st)) {
+                int count = counterMap.get(st);
+                counterMap.put(st, count + 1);
+            } else{
+                counterMap.put(st,1);
+            }
+        }
+
+        System.out.println("Occurrences of Words in String : "+counterMap);
     }
 }
