13ace14257e5e997bc4645bdb57dbfbdc2ccecbd
Sum7894 <159606495+Sum7894@users.noreply.github.com>
2024-02-17 16:48:07 +0530

    Program for counting occurrences of characters in a string

--- BasicJavaPrep/src/main/java/org/example/Main.java
+++ BasicJavaPrep/src/main/java/org/example/Main.java
@@ -1,17 +1,39 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/** Count occurrences of charcters in a string
+ * for eg : ababeu -  a = 2 , b = 2, e = 1, u = 1
+ *
+ * **/
+
+
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner sin = new Scanner(System.in);
+        String inputSt = sin.nextLine();
+        Main m = new Main();
+        m.countOcc(inputSt.trim());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    }
+
+    public void countOcc(String str){
+        Map<Character, Integer> counterMap = new HashMap<>();
+        char[] chArr = str.toCharArray();
+
+        for(char ch : chArr){
+            if(counterMap.containsKey(ch)) {
+                int prev = counterMap.get(ch);
+                counterMap.put(ch, prev+1);
+            }else{
+                counterMap.put(ch,1);
+            }
         }
+         counterMap.remove(' ');
+         System.out.println("MAP  : "+counterMap);
     }
 }
